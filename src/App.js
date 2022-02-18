3a32e3537b0d1c4219fb86c91c91a749095ddc3b
THATOMAS <chachumashori@gmail.com>
2022-02-18 16:09:58 +0200

    Modified the App and made it more minimalistic.At this point the is a bug however, when you minimize the window vertically the componets shift.

--- src/App.js
+++ src/App.js
@@ -5,19 +5,18 @@ import {logo,centerImage,cursor,miniLogo,topCursor,pageText}  from "./SVG";
 
 const App = ()=>{
   return(
-<main>
-    <section className="FirstPage">
+<main className="container">
+    <section >
       <div className="brand-logo">{logo()}</div>
       <div className="center-image"><img src={require("./assets/SVGs/centerpic.jpg")} alt=""/></div>
       <div className="cursor">{cursor()}</div>
+     
     </section>
-
-    <section className="SecondPage">
-      <div className="mini-logo">{miniLogo()}</div>
-      <div className="topCursor">{topCursor()}</div>
+    <section>
+     <div classname="topCursor">{topCursor()}</div>
+     <div className="mini-logo">{miniLogo()}</div>
+      <div className="pageText">{pageText()}</div>
     </section>
-    <div className="pageText">{pageText()}</div>
-
 </main>)
 
 }
