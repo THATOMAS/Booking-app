eddc3b641171b736903ee8a89f850ea505a59638
THATOMAS <chachumashori@gmail.com>
2022-02-18 11:11:59 +0200

    Completed the snap effect and placed all the SVG files accordingly

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
 import React from "react"
 import "./index.css"
 
-import {logo,centerImage,cursor}  from "./SVG";
+import {logo,centerImage,cursor,miniLogo,topCursor,pageText}  from "./SVG";
 
 const App = ()=>{
   return(
-    <main>
+<main>
     <section className="FirstPage">
-      <div className="brand-logo">
-        {logo()}
-      </div>
-      
-      <div className="center-image">
-        {centerImage()}
-      </div>
-      <div className="cursor">
-        {cursor()}
-      </div>
+      <div className="brand-logo">{logo()}</div>
+      <div className="center-image"><img src={require("./assets/SVGs/centerpic.jpg")} alt=""/></div>
+      <div className="cursor">{cursor()}</div>
     </section>
-    <section className="SecondPage">
 
+    <section className="SecondPage">
+      <div className="mini-logo">{miniLogo()}</div>
+      <div className="topCursor">{topCursor()}</div>
     </section>
-    
-    </main>)
+    <div className="pageText">{pageText()}</div>
+
+</main>)
 
 }
 
