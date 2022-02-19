839b5296e164199762f696b434a60e70cf0f4de8
THATOMAS <chachumashori@gmail.com>
2022-02-19 16:04:01 +0200

    Fixed the line heights and adjusted them to make the snap effect scroll functional,At this point i am yet to add the a tag to link us to the actual wwebsite.

--- src/App.js
+++ src/App.js
@@ -6,17 +6,20 @@ import {logo,centerImage,cursor,miniLogo,topCursor,pageText}  from "./SVG";
 const App = ()=>{
   return(
 <main className="container">
-    <section >
+    <section className="FirstPage">
       <div className="brand-logo">{logo()}</div>
       <div className="center-image"><img src={require("./assets/SVGs/centerpic.jpg")} alt=""/></div>
       <div className="cursor">{cursor()}</div>
      
     </section>
-    <section>
-     <div classname="topCursor">{topCursor()}</div>
-     <div className="mini-logo">{miniLogo()}</div>
-      <div className="pageText">{pageText()}</div>
+    
+    <section className="SecondPage">
+      <div classname="topCursor">{topCursor()}</div>
+      <div className="mini-logo">{miniLogo()}</div>
+      <div className="pageText">{pageText()}</div>  
     </section>
+ 
+
 </main>)
 
 }
