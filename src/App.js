dfd65a5a73e08d181b0a0e68fcfb1a3649b65405
THATOMAS <chachumashori@gmail.com>
2022-02-19 16:55:14 +0200

    Added a txt file explaining the logic behind the scroll on click functionality .At this point all i need to do is make a link to the actual website.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
-import React from "react"
+import React,{ useRef } from "react"
 import "./index.css"
 
 import {logo,centerImage,cursor,miniLogo,topCursor,pageText}  from "./SVG";
 
+
 const App = ()=>{
+
+  const titleRef = useRef()
+  const titleRef2 = useRef()
+
+  function toTop () {
+  titleRef.current.scrollIntoView({behavior:"smooth"})
+}
+
+
+function toBottom () {
+  titleRef2.current.scrollIntoView({behavior:"smooth"})
+}
+
+
   return(
 <main className="container">
-    <section className="FirstPage">
+    <section className="FirstPage" id="FirstPage" ref={titleRef}>
       <div className="brand-logo">{logo()}</div>
       <div className="center-image"><img src={require("./assets/SVGs/centerpic.jpg")} alt=""/></div>
-      <div className="cursor">{cursor()}</div>
+      <div className="cursor" onClick={toBottom}>{cursor()}</div>
      
     </section>
     
-    <section className="SecondPage">
-      <div classname="topCursor">{topCursor()}</div>
+    <section className="SecondPage" id="SecondPage" ref={titleRef2}>
+      <div classname="topCursor" onClick={toTop}>{topCursor()}</div>
       <div className="mini-logo">{miniLogo()}</div>
       <div className="pageText">{pageText()}</div>  
     </section>
