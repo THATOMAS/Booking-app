64b26bfb578a0e44a3dd3aeb7e8a742fb98ab6d2
THATOMAS <chachumashori@gmail.com>
2022-05-12 15:48:52 +0200

    Finished with page infomation box at the center of the welcom home page

--- src/routes/WelcomePage.jsx
+++ src/routes/WelcomePage.jsx
@@ -2,7 +2,37 @@ import React from "react";
 
 const WelcomePage = () => {
     return <section>
-      <h1>Welcome Page</h1>
+      <article id="welcome-page">
+        <div id='info-box'>
+          <div id='page-heading-box'>
+            <div id='heading-stroke' style={{marginLeft:"0.4em"}}></div>
+            <h3 style={{fontFamily:'Quicksand',
+            fontSize:'2.5em',
+            marginLeft:"0.4em"
+          }}>You'll be buying the <br/>best products.</h3>
+          </div>
+          
+          <p id='paragraph-text'>Great raw materials,exceptional quality and 
+unperalleled craftsmanship is what we give you.<br/><br/>
+With our experince in the furniture business 
+you will get awesome customer service .</p>
+
+  <div id='underline'></div>
+
+  <div id='nearest-store'>
+    <p style={{fontFamily:'Quicksand',
+    fontSize:'2em',
+    color:'white',
+    cursor:'pointer'
+
+  }}>Nearest Store</p>
+  </div>
+        </div>
+
+        
+        
+        <div id="photo-box"></div>
+      </article>
       </section>;
   
 }
